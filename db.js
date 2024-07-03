6df9cca4a5bd9e029ed20745ca66f096066ffdcd
Pedro Medeiros <medeirospedro083@gmail.com>
2024-07-03 01:04:12 -0300

    feat: add dataset words

---
+++ db.js
@@ -0,0 +1,13 @@
+const words = [
+  "agua", "cabelo", "caminho", "passado", "segunda", "viagem", "espelho", "corpo", "alma", "roupa", "mundo", "cozinha", "pessoa", "idade", "cabeca", "ouvido", "chuva", "papel", "gelo", "frio", "verao", "calor", "estrela", "vento", 
+  "nuvem", "terra", "sombra", "areia", "pedra", "fogo", "porta", "janela", "cofre", "bola", "arvore", "planta", "animal", "cachorro", "gato", "rei", "rainha", "dente", "nariz", "labio", "orelha", "lingua", "barriga", "ombro", "joelho", "perna", 
+  "bunda", "costa", "cinto", "botao", "meia", "calca", "sapato", "chapeu", "camisa", "vestido", "carro", "casa", "aviao", "barco", "navio", "trem", "onibus", "estrada", "ponte", "predio", "cidade", "aldeia", "pais", "lua", "sol", 
+  "agua", "terra", "fogo", "calor", "frio", "vento", "chuva", "neve", "nuvem", "ceu", "sol", "lua", "corpo", "cabeca", "coracao", "pele", "ossos", "musculo", "sangue", "pulmao", "estomago", "figado", "rins", "bexiga", "dente", 
+  "nariz", "orelha", "olho", "barriga", "costa", "peito", "ombro", "cintura", "coxa", "bunda", "cabelo", "roupa", "camisa", "calca", "shorts", "vestido", "saia", "chapeu", "luva", "meia", "sapato", "chinelo", "mala", "carteira", "celular", "livro", 
+  "caneta", "lapis", "giz", "lousa", "mapa", "bussola", "aviao", "barco", "carro", "onibus", "trem", "taxi", "escola", "creche", "medico", "escola", "creche", "filme", "ator", "atriz", "cena", "palco", "museu", "obra", "escultura", "pintura", "desenho", 
+  "rascunho", "projeto", "plano", "ideia", "sonho", "verdade", "mentira", "certo", "errado", "bom", "ruim", "feio", "grande", "pequeno", "alto", "baixo", "duro", "quente", "frio", "seco", "escuro", "claro", "preto", "branco", "cinza", "verde", "azul", 
+  "roxo", "rosa", "marrom", "bege"
+];
+
+export { words }
+
